c3a75939b79b5ec6632630db6175eb0e7c94994d
DJDREAMNZ <ur2l8m8@gmail.com>
2020-09-12 05:04:47 +1200

    Added see post.

---
+++ src/components/SeePost.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+class ViewPost extends React.Component {
+  render() {
+    return (
+      <div>
+       <h2>This is where you view a post</h2>
+      </div>
+    )
+  }
+}
+
+export default ViewPost;
