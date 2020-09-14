43afbcd152c4e872f471f0ed3fea433b4b6a8a0a
DJDREAMNZ <ur2l8m8@gmail.com>
2020-09-14 14:36:39 +1200

    Fixed routing, ready to begin styling.

---
+++ src/components/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+class LandingComp extends React.Component {
+  render() {
+    return (
+      <div>
+       <h2>Welcome to the landing page</h2>
+      </div>
+    )
+  }
+}
+
+export default LandingComp;
--- src/index.js
+++ src/index.js
@@ -5,19 +5,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import CreatePostComp from './components/CreatePost';
-import EditPostComp from './components/EditPost';
-import NewsfeedPosts from './components/Newsfeed';
+import EditPostComp from './components/EditPost'; 
 import LoginComp from './components/Login';
 import ViewPost from './components/SeePost';
+import LandingComp from './components/LandingPage';
 
 ReactDOM.render(
   <Router>
       <div>
-        <Route exact path='/' component={App} />
+        <Route exact path='/' component={LandingComp} />
         <Route path='/edit/:id' component={EditPostComp} />
         <Route path='/create' component={CreatePostComp} />
         <Route path='/show/:id' component={ViewPost} />
         <Route path='/login' component={LoginComp} />
+        <Route path='/newsfeed' component={App} />
       </div>
   </Router>,
   document.getElementById('root')
