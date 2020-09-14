76e7979f652c09b370a3c9293a8716df113bef6a
anthonyhong2020 <anthony020674@gmail.com>
2020-09-14 16:10:13 +1200

    Created first-page and newsfeed page.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,107 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        This is where the GET request goes. The main newsfeed page. Posts display here.
-        </p>
-      </header>
+    <div className="app">
+<div className="newsfeed-page">
+  <div className="gradient" />
+  <div className="fixedtop">
+    <div className="nav-top">
+      <div className="update-profile">
+        <i className="far fa-edit" />
+      </div>
+      <div className="heading">
+        <div className="d">
+          D<div className="daily">aily</div>
+        </div>
+        <div className="a">
+          A<div className="architecture">rchitecture</div>
+        </div>
+        <div className="p">
+          P<div className="post">ost</div>
+        </div>
+      </div>
+    </div>
+    <div className="main-profilepic">
+      <img src="asset\profile1.jpg" alt />
+      <div className="email-updates">
+        sarah2020@gmail.com
+        <div className="profile-edit"></div>
+      </div>
+    </div>
+    <div className="fixed-profile">
+      <div className="tab1tab2">
+        <div className="first-backgroundimg"></div>
+      </div>
+    </div>
+  </div>
+  <div className="post-container">
+    <div className="posts">
+      <div className="post-group">
+        <div className="breakline" />
+        <div className="profile-title-location">
+          <div className="post-profilepic">
+            <img src="asset\profile1.jpg" alt />
+          </div>
+          <div className="title-of-photo">Twin Tower</div>
+          <div className="location">
+            <div className="location-thumbtack">
+              <i className="fas fa-thumbtack" />
+            </div>
+            New York City, USA
+          </div>
+        </div>
+        <div className="posted-image">
+          <img src="asset\Pattern.jpg" alt />
+        </div>
+        <div className="edit-like-fav">
+          <div className="left">
+            <i className="fas fa-edit" />
+            <i className="fas fa-heart" />
+            <i className="fas fa-star" />
+          </div>
+          <div className="right">
+            <i className="fas fa-trash" />
+          </div>
+        </div>
+        <div className="comment comment1">
+          <img src="asset\profile3.jpg" alt />
+          <div className="user-comment1">Wow! Nice view of the building.</div>
+        </div>
+        <div className="comment comment2">
+          <img src="asset\profile3.jpg" alt />
+          <div className="user-comment2">What time did you take the photo?</div>
+        </div>
+        <div className="comment comment3">
+          <img src="asset\profile1.jpg" alt />
+          <form action className="comment-form">
+            <input
+              type="text"
+              id="comment-input"
+              placeholder="Add a comment ..."
+            />
+            <button type="submit" id="comment-post">
+              Post
+            </button>
+          </form>
+        </div>
+        <div className="bottom-breakline" />
+        <div className="footer">
+          <div className="footer-gradient" />
+          <div className="tab1">
+            <i className="fas fa-building" />
+          </div>
+          <div className="tab2">
+            <i className="fas fa-plus" />
+          </div>
+          <div className="tab3">
+            <i className="fas fa-star" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,9 +5,44 @@ import React from 'react';
 class LoginComp extends React.Component {
   render() {
     return (
-      <div>
-       <h2>This is anthony's first test route</h2>
-      </div>
+<div className="login login-page">
+  <div className="first-backgroundimg"></div>
+  <div className="blackbackground"></div>
+  <div className="container-login">
+    <div className="loginBox">
+      <h2>Login</h2>
+      <form action className="login-input">
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-input"
+            name="username-input"
+            id="username-input"
+            placeholder="Username (email)"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-input"
+            name="password-input"
+            id="password-input"
+            placeholder="Password / Create New Password"
+          />
+        </div>
+        <div className="signupwith">Don't have an account? Sign up with</div>
+        <div className="buttons">
+          <button type="button" className="btn btn-google">
+            Google
+          </button>
+          <button type="button" className="btn btn-done">
+            Done
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
     )
   }
 }
