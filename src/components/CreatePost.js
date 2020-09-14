f380191f88342d176d47177492a3edf0b3165b3c
DJDREAMNZ <ur2l8m8@gmail.com>
2020-09-14 15:45:31 +1200

    Implemented Anthony's Prototype To React + HTML to JSX
    I took the code from Anthony's prototype he made, and implemented it into the correct Components and made the JSX conversion. Plus added CSS from the prototype.

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -5,9 +5,63 @@ import React from 'react';
 class CreatePostComp extends React.Component {
   render() {
     return (
-      <div>
-       <h2>This is where you create a post</h2>
+      <div className="addpost addpost-container">
+      <div className="add-new-posts">
+        <div className="close">
+          <i className="fas fa-window-close" />
+        </div>
+        <div className="add-a-new-post">- Add a New Post -</div>
+        <form action>
+          <div className="form-group2">
+            <input
+              type="text"
+              className="title-photo"
+              name="title-input"
+              id="title-input"
+              placeholder="Name of Building"
+            />
+          </div>
+          <div className="form-group2">
+            <input
+              type="text"
+              className="location-photo"
+              name="location-input"
+              id="location-input"
+              placeholder="Location"
+            />
+          </div>
+          <div className="form-group2">
+            <button className="inside-input">
+              <i className="fas fa-plus" />
+            </button>
+            <input
+              type="text"
+              className="upload-images"
+              name="location-input"
+              id="location-input"
+              placeholder=" Upload images"
+            />
+          </div>
+          <div className="buttons-addpostdone">
+            <button type="button" className="btn btn-newpostdone">
+              Done
+            </button>
+          </div>
+        </form>
       </div>
+      <div className="footer">
+        <div className="footer-gradient" />
+        <div className="tab-tab1">
+          <i className="fas fa-building" />
+        </div>
+        <div className="tab-tab2">
+          <i className="fas fa-plus" />
+        </div>
+        <div className="tab-tab3">
+          <i className="fas fa-star" />
+        </div>
+      </div>
+    </div>
     )
   }
 }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,9 +5,17 @@ import React from 'react';
 class LandingComp extends React.Component {
   render() {
     return (
-      <div>
-       <h2>Welcome to the landing page</h2>
+      <div className="first first-page">
+      <div className="first-backgroundimg"></div>
+      <div className="blackbackground"></div>
+      <div className="container">
+        <div className="appname">
+          <h1>D A P</h1>
+          <p>-Let your imagination go wild-</p>
+        </div>
+        <div className="taptobegin">Tap anywhere to begin</div>
       </div>
+    </div>
     )
   }
 }
