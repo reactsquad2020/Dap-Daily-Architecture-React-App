84ce8efed722b5e4942cf14c53bc7d1eb5eef864
anthonyhong2020 <anthony020674@gmail.com>
2020-09-16 14:42:12 +1200

    Additional changes on css

--- src/components/Feeds.js
+++ src/components/Feeds.js
@@ -73,15 +73,15 @@ class FeedsComp extends React.Component {
                 </div>
                 <div className="comment comment1">
                   <img src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/277-pom-009777-chim.jpg?w=1000&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=83bc3f756995eab9c6176211c3fbee62"  />
-                  <div className="user-comment1">Wow! Nice view of the building.</div>
+                  <div className="user user-comment1">Wow! Nice view of the building.</div>
                 </div>
                 <div className="comment comment2">
                   <img src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/277-pom-009777-chim.jpg?w=1000&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=83bc3f756995eab9c6176211c3fbee62"  />
-                  <div className="user-comment2">What time did you take the photo?</div>
+                  <div className="user user-comment2">What time did you take the photo?</div>
                 </div>
                 <div className="comment comment3">
                   <img src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/k-56-dsc2965456645345639.jpg?w=1000&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=db42e7af25ea1c609baa0f34408a9fce"  />
-                  <form action className="comment-form">
+                  <form action className="user comment-form">
                     <input
                       type="text"
                       id="comment-input"
